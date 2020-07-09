74acd37628dc7bd00de0b4858d20cab07d95e4b9
John Brunton <john_brunton@hotmail.co.uk>
2020-07-09 20:48:26 +0100

    refactor tests

--- jsonnet/jsonnet_test.go
+++ jsonnet/jsonnet_test.go
@@ -8,52 +8,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMarshalJsonnetEmpty(t *testing.T) {
-	v := make(map[string]interface{})
-	json.Unmarshal([]byte{}, &v)
-	out, err := MarshalJsonnet(v)
-	assert.Equal(t, "{}", string(out))
-	assert.NoError(t, err)
-}
+func TestMarshalJsonnet(t *testing.T) {
+	testCases := []struct {
+		jsonInput       string
+		expectedJsonnet string
+	}{
+		{
+			jsonInput:       "{}",
+			expectedJsonnet: "{}",
+		},
+		{
+			jsonInput: `{
+				"foo": "bar",
+				"baz": 123
+			}`,
+			expectedJsonnet: strings.Join([]string{
+				`{`,
+				`  baz: 123,`,
+				`  foo: "bar"`,
+				`}`,
+			}, "\n"),
+		},
+		{
+			jsonInput: `{
+				"foo": "bar",
+				"if": "keyword",
+				"key-with-dashes": 123,
+				"key with spaces": 456
+			}`,
+			expectedJsonnet: strings.Join([]string{
+				`{`,
+				`  foo: "bar",`,
+				`  "if": "keyword",`,
+				`  "key with spaces": 456,`,
+				`  "key-with-dashes": 123`,
+				`}`,
+			}, "\n"),
+		},
+	}
 
-func TestMarshalJsonnetSimple(t *testing.T) {
-	v := make(map[string]interface{})
-	json.Unmarshal([]byte(`{"foo": "bar", "baz": 123}`), &v)
-	out, err := MarshalJsonnet(v)
-	expected := strings.Join([]string{
-		`{`,
-		`  baz: 123,`,
-		`  foo: "bar"`,
-		`}`,
-	}, "\n")
-	assert.Equal(t, expected, string(out))
-	assert.NoError(t, err)
-}
-
-func TestMarshalJsonnetKeywords(t *testing.T) {
-	v := make(map[string]interface{})
-	json.Unmarshal([]byte(`{"foo": "bar", "if": "${{ github.expression }}" }`), &v)
-	out, err := MarshalJsonnet(v)
-	expected := strings.Join([]string{
-		`{`,
-		`  foo: "bar",`,
-		`  "if": "${{ github.expression }}"`,
-		`}`,
-	}, "\n")
-	assert.Equal(t, expected, string(out))
-	assert.NoError(t, err)
-}
-
-func TestMarshalJsonnetComplexKeys(t *testing.T) {
-	v := make(map[string]interface{})
-	json.Unmarshal([]byte(`{"key-with-dashes": 123,"key with spaces": 456}`), &v)
-	out, err := MarshalJsonnet(v)
-	expected := strings.Join([]string{
-		`{`,
-		`  "key with spaces": 456,`,
-		`  "key-with-dashes": 123`,
-		`}`,
-	}, "\n")
-	assert.Equal(t, expected, string(out))
-	assert.NoError(t, err)
+	for _, testCase := range testCases {
+		v := make(map[string]interface{})
+		json.Unmarshal([]byte(testCase.jsonInput), &v)
+		out, err := MarshalJsonnet(v)
+		assert.Equal(t, testCase.expectedJsonnet, string(out))
+		assert.NoError(t, err)
+	}
 }
