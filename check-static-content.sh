82fb2e5e412cb6be03df1ed1e3ccb342b6c9695c
John Brunton <john_brunton@hotmail.co.uk>
2020-07-22 19:12:38 +0100

    fix check-static-content script

--- check-static-content.sh
+++ check-static-content.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-make statik
+function assert_no_changes() {
+  if [[ `git status --porcelain` ]]; then
+    echo $1
+    exit 1
+  else
+    echo $2
+  fi
+}
 
-if [[ `git status --porcelain` ]]; then
-  echo "Static content is out of date. Run \"make statik\" and commit the changes."
-  exit 1
-else
-  echo "Static content is up to date."
-fi
+assert_no_changes "Dirty repo, commit changes before running this command"
+
+make statik
+assert_no_changes "Static content is out of date. Run \"make statik\" and commit the changes." \
+  "Static content is up to date."
 
 make build
-
-./gflows init
-
-if [[ `git status --porcelain` ]]; then
-  echo "Default workflow files don't match generated files. Contents of static-content and .gflows need to match."
-  exit 1
-else
-  echo "Default workflow files are up to date."
-fi
+./gflows init ytt
+assert_no_changes "Default workflow files don't match generated files. Contents of static-content and .gflows need to match." \
+  "Default workflow files are up to date."
