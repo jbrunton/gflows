6f203aa67f4154fb19ff539de512e76191474aa4
John Brunton <john_brunton@hotmail.co.uk>
2020-07-23 16:07:50 +0100

    add unit tests for YamlToJson

---
+++ yaml/yaml_to_json_test.go
@@ -0,0 +1,59 @@
+package yaml
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"encoding/json"
+)
+
+func TestYamlToJson(t *testing.T) {
+	scenarios := []struct {
+		description   string
+		yaml          string
+		expectedError string
+		expectedJson  string
+	}{
+		{
+			description:  "empty file",
+			yaml:         "",
+			expectedJson: "{}",
+		},
+		{
+			description:  "simple yaml",
+			yaml:         "foo: bar",
+			expectedJson: `{"foo":"bar"}`,
+		},
+		{
+			description:  "on keyword",
+			yaml:         "on: foo",
+			expectedJson: `{"on":"foo"}`,
+		},
+		{
+			description:   "key error",
+			yaml:          "123: foo",
+			expectedError: "Non-string key at top level: 123",
+		},
+		{
+			description:   "invalid yaml",
+			yaml:          "&",
+			expectedError: "yaml: did not find expected alphabetic or numeric character",
+		},
+	}
+
+	for _, scenario := range scenarios {
+		jsonData, err := YamlToJson(scenario.yaml)
+		if scenario.expectedError == "" {
+			assert.NoError(t, err, "Unexpected error in scenario %q", scenario.description)
+
+			jsonBytes, err := json.Marshal(jsonData)
+			assert.NoError(t, err, "Unexpected error in scenario %q", scenario.description)
+
+			jsonString := string(jsonBytes)
+			assert.Equal(t, scenario.expectedJson, jsonString, "Unexpected json in scenario %q", scenario.description)
+		} else {
+			assert.EqualError(t, err, scenario.expectedError, "Unexpected error in scenario %q", scenario.description)
+		}
+	}
+}
