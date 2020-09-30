8db109b6311bb590a0e6437bb12bc7bebbbf29ad
John Brunton <john_brunton@live.co.uk>
2020-09-30 14:18:13 +0100

    fix tests

---
+++ fixtures/test_round_tripper.go
@@ -0,0 +1,47 @@
+package fixtures
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type TestRoundTripper struct {
+	responses map[string]*http.Response
+}
+
+func (roundTripper *TestRoundTripper) RoundTrip(request *http.Request) (*http.Response, error) {
+	url := request.URL.String()
+	response := roundTripper.responses[url]
+	if response == nil {
+		return nil, fmt.Errorf("Missing response for %s", url)
+	}
+	return response, nil
+}
+
+func (roundTripper *TestRoundTripper) StubResponse(url string, response *http.Response) {
+	roundTripper.responses[url] = response
+}
+
+func (roundTripper *TestRoundTripper) StubBody(url string, responseBody string) {
+	roundTripper.StubResponse(url, &http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBufferString(responseBody)),
+		Header:     make(http.Header),
+	})
+}
+
+func (roundTripper *TestRoundTripper) StubStatusCode(url string, statusCode int) {
+	roundTripper.StubResponse(url, &http.Response{
+		StatusCode: statusCode,
+		Body:       ioutil.NopCloser(bytes.NewBufferString("")),
+		Header:     make(http.Header),
+	})
+}
+
+func NewTestRoundTripper() *TestRoundTripper {
+	return &TestRoundTripper{
+		responses: make(map[string]*http.Response),
+	}
+}
