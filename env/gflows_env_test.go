55cef4b3be21db264ff95e6e75f4f5b7ea7d91fa
John Brunton <john_brunton@live.co.uk>
2020-10-02 20:54:52 +0100

    add test for GetLibPaths()

--- env/gflows_env_test.go
+++ env/gflows_env_test.go
@@ -3,6 +3,7 @@ package env
 import (
 	"net/http"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,8 +12,8 @@ import (
 	"github.com/jbrunton/gflows/io/content"
 )
 
-func newTestEnv(roundTripper http.RoundTripper) (*GFlowsEnv, *content.Container) {
-	ioContainer, context, _ := fixtures.NewTestContext("")
+func newTestEnv(config string, roundTripper http.RoundTripper) (*GFlowsEnv, *content.Container) {
+	ioContainer, context, _ := fixtures.NewTestContext(config)
 	httpClient := &http.Client{Transport: roundTripper}
 	container := content.NewContainer(ioContainer, httpClient)
 	installer := NewGFlowsLibInstaller(container.FileSystem(), container.ContentReader(), container.ContentWriter(), container.Logger())
@@ -21,7 +22,7 @@ func newTestEnv(roundTripper http.RoundTripper) (*GFlowsEnv, *content.Container)
 }
 
 func TestLoadLocalLibrary(t *testing.T) {
-	env, container := newTestEnv(fixtures.NewMockRoundTripper())
+	env, container := newTestEnv("", fixtures.NewMockRoundTripper())
 	fs := container.FileSystem()
 	container.ContentWriter().SafelyWriteFile("/path/to/my-lib.gflowslib", `{"files": ["libs/lib.yml"]}`)
 	container.ContentWriter().SafelyWriteFile("/path/to/libs/lib.yml", "foo: bar")
@@ -37,7 +38,7 @@ func TestLoadLocalLibrary(t *testing.T) {
 
 func TestLoadRemoteLib(t *testing.T) {
 	roundTripper := fixtures.NewMockRoundTripper()
-	env, container := newTestEnv(roundTripper)
+	env, container := newTestEnv("", roundTripper)
 	fs := container.FileSystem()
 	roundTripper.StubBody("https://example.com/path/to/my-lib.gflowslib", `{"files": ["libs/lib.yml"]}`)
 	roundTripper.StubBody("https://example.com/path/to/libs/lib.yml", "foo: bar")
@@ -53,7 +54,7 @@ func TestLoadRemoteLib(t *testing.T) {
 
 func TestCacheRemoteLibs(t *testing.T) {
 	roundTripper := fixtures.NewMockRoundTripper()
-	env, _ := newTestEnv(roundTripper)
+	env, _ := newTestEnv("", roundTripper)
 	roundTripper.StubBody("https://example.com/path/to/my-lib.gflowslib", `{"files": ["libs/lib.yml"]}`)
 	roundTripper.StubBody("https://example.com/path/to/libs/lib.yml", "foo: bar")
 
@@ -69,7 +70,7 @@ func TestCacheRemoteLibs(t *testing.T) {
 
 func TestGetPackages(t *testing.T) {
 	// arrange
-	env, container := newTestEnv(fixtures.NewMockRoundTripper())
+	env, container := newTestEnv("", fixtures.NewMockRoundTripper())
 	container.ContentWriter().SafelyWriteFile("/path/to/my-lib.gflowslib", `{"files": ["libs/lib.yml"]}`)
 	container.ContentWriter().SafelyWriteFile("/path/to/libs/lib.yml", "foo: bar")
 	lib, _ := env.LoadDependency("/path/to/my-lib.gflowslib")
@@ -90,78 +91,29 @@ func TestGetPackages(t *testing.T) {
 	assert.Equal(t, filepath.Join(lib.LocalDir, "libs"), libPackage.LibsDir())
 }
 
-// func TestGetJPath(t *testing.T) {
-// 	config := strings.Join([]string{
-// 		"templates:",
-// 		"  engine: jsonnet",
-// 		"  defaults:",
-// 		"    libs: [some-lib]",
-// 		"  overrides:",
-// 		"    my-workflow:",
-// 		"      libs: [my-lib]",
-// 	}, "\n")
-// 	_, _, engine := newJsonnetTemplateEngine(config, fixtures.NewMockRoundTripper())
+func TestGetLibPaths(t *testing.T) {
+	config := strings.Join([]string{
+		"templates:",
+		"  engine: jsonnet",
+		"  defaults:",
+		"    libs: [/libs/some-lib]",
+		"    dependencies: [/deps/some-pkg/some-pkg.gflowslib]",
+		"  overrides:",
+		"    my-workflow:",
+		"      libs: [/libs/my-lib]",
+		"      dependencies: [/deps/my-pkg/my-pkg.gflowslib]",
+	}, "\n")
+	env, container := newTestEnv(config, fixtures.NewMockRoundTripper())
+	container.ContentWriter().SafelyWriteFile("/deps/some-pkg/some-pkg.gflowslib", `{"files": []}`)
+	container.ContentWriter().SafelyWriteFile("/deps/my-pkg/my-pkg.gflowslib", `{"files": []}`)
+	somePkg, _ := env.LoadDependency("/deps/some-pkg/some-pkg.gflowslib")
+	myPkg, _ := env.LoadDependency("/deps/my-pkg/my-pkg.gflowslib")
 
-// 	jpath, _ := engine.getJPath("my-workflow")
-// 	assert.Equal(t, []string{".gflows/some-lib", ".gflows/my-lib"}, jpath)
+	paths, err := env.GetLibPaths("my-workflow")
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"/libs/some-lib", "/libs/my-lib", somePkg.LibsDir(), myPkg.LibsDir()}, paths)
 
-// 	jpath, _ = engine.getJPath("other-workflow")
-// 	assert.Equal(t, []string{".gflows/some-lib"}, jpath)
-// }
-
-// func TestJPathErrors(t *testing.T) {
-// 	roundTripper := fixtures.NewMockRoundTripper()
-// 	roundTripper.StubStatusCode("https://example.com/my-lib.gflowslib", 500)
-// 	config := strings.Join([]string{
-// 		"templates:",
-// 		"  engine: jsonnet",
-// 		"  defaults:",
-// 		"    libs: [https://example.com/my-lib.gflowslib]",
-// 	}, "\n")
-// 	_, _, engine := newJsonnetTemplateEngine(config, roundTripper)
-
-// 	_, err := engine.getJPath("my-workflow")
-// 	assert.EqualError(t, err, "Received status code 500 from https://example.com/my-lib.gflowslib")
-// }
-
-// func TestGetYttLibs(t *testing.T) {
-// 	config := strings.Join([]string{
-// 		"templates:",
-// 		"  engine: ytt",
-// 		"  defaults:",
-// 		"    libs: [common, config]",
-// 		"  overrides:",
-// 		"    my-workflow:",
-// 		"      libs: [my-lib]",
-// 	}, "\n")
-// 	_, _, engine, _ := newYttTemplateEngine(config)
-
-// 	paths, err := engine.getYttLibs("my-workflow")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, []string{".gflows/common", ".gflows/config", ".gflows/my-lib"}, paths)
-
-// 	paths, err = engine.getYttLibs("other-workflow")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, []string{".gflows/common", ".gflows/config"}, paths)
-// }
-
-// func TestRemoteLibs(t *testing.T) {
-// 	config := strings.Join([]string{
-// 		"templates:",
-// 		"  engine: ytt",
-// 		"  defaults:",
-// 		"    libs: [https://example.com/my-lib.gflowslib]",
-// 		"  overrides:",
-// 		"    my-workflow:",
-// 		"      libs: [https://example.com/other-lib.gflowslib]",
-// 	}, "\n")
-// 	_, _, engine, roundTripper := newYttTemplateEngine(config)
-// 	roundTripper.StubBody("https://example.com/my-lib.gflowslib", `{"files":[]}`)
-// 	roundTripper.StubBody("https://example.com/other-lib.gflowslib", `{"files":[]}`)
-
-// 	paths, err := engine.getYttLibs("my-workflow")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, len(paths), 2)
-// 	assert.Regexp(t, "my-lib.gflowslib[0-9]+/libs$", paths[0])
-// 	assert.Regexp(t, "other-lib.gflowslib[0-9]+/libs$", paths[1])
-// }
+	paths, err = env.GetLibPaths("other-workflow")
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"/libs/some-lib", somePkg.LibsDir()}, paths)
+}
